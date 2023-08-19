53650b46d3d75ab4ae5b8b5a647b7b1f2d59926d
Alvin Maulana <alvin.maulana@ccn.com.sg>
2023-08-19 13:50:17 +0800

    test deploy

--- astroWeb/astro.config.mjs
+++ astroWeb/astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://alvinmlna.github.io',
   base: '/portofolio',
+  trailingSlash: 'always', // Use to always append '/' at end of url
 })
--- astroWeb/src/pages/page-1.md
+++ astroWeb/src/pages/page-1.md
@@ -1,5 +1,5 @@
 ---
-layout: ../../templates/BasePostLayout.astro
+layout: ../templates/BasePostLayout.astro
 title: Sample page
 
 ---
